eddabe8d3494a4cb281f982685874123a711e58a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-08-07 02:46:00 +0300

    Add documentation to locales.dart

--- lib/src/settings/locales.dart
+++ lib/src/settings/locales.dart
@@ -1,16 +1,23 @@
 import 'dart:ui' show Locale;
 
+import 'package:flutter/material.dart';
+
 const Locale en_US = Locale('en', 'US');
-
 const Locale tr_TR = Locale('tr', 'TR');
-
 const Map<String, Locale> _locales = <String, Locale>{
   'en-US': en_US,
   'tr-TR': tr_TR
 };
 
-Locale getLocale(String locale) => _locales[locale] ?? en_US;
+/// Returns the given [localeString] as [Locale] object.
+///
+/// e.g. If the given [localeString] is `en-US` then `Locale('en', 'US')` will
+/// be returned.
+Locale getLocale(String localeString) => _locales[localeString] ?? en_US;
 
+/// Returns the supported locales.
+///
+/// At the moment, supported locales are `en_US` and `tr_TR`.
 const List<Locale> supportedLocales = <Locale>[
   en_US,
   tr_TR,
