a70af4915610cde4034bd309bd887d59f7ebe1f0
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-03 19:50:08 +0300

    Use .split() instead of describeEnum

--- lib/src/models/vote_type.dart
+++ lib/src/models/vote_type.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 enum VoteType { upVote, downVote }
 
 extension AsString on VoteType {
-  String get asString => describeEnum(this);
+  String get asString => toString().split('.').last;
 }
