6ba6a8fc2077b49ae0a0c456fd023d2107ea892f
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-13 19:34:13 +0300

    Ignore unnecessary_string_escapes in messages_en.dart

--- lib/src/l10n/messages_en.dart
+++ lib/src/l10n/messages_en.dart
@@ -4,6 +4,7 @@
 // function name.
 
 // Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_string_escapes
 // ignore_for_file:unnecessary_brace_in_string_interps
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
