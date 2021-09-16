98373874735387b9d9ca1e944f8895e279228ff8
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-16 17:59:39 +0300

    Implement CrashlyticsPrinter onLog()

--- lib/src/utils/crashlytics_printer.dart
+++ lib/src/utils/crashlytics_printer.dart
@@ -1,8 +1,14 @@
-import 'package:loggy/loggy.dart' show LogRecord, LoggyPrinter;
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:loggy/loggy.dart' show LogLevel, LogRecord, LoggyPrinter;
 
 class CrashlyticsPrinter extends LoggyPrinter {
   const CrashlyticsPrinter() : super();
 
   @override
-  void onLog(LogRecord record) {}
+  void onLog(LogRecord record) {
+    if (record.level == LogLevel.error) {
+      // Pass all errors to Crashlytics.
+      FirebaseCrashlytics.instance.recordError(record.error, record.stackTrace);
+    }
+  }
 }
