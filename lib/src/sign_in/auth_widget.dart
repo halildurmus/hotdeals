0901fb1a8026beb2d64d48e7856acb69c0cbfd0a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-25 00:27:59 +0300

    Update AuthWidget

--- lib/src/sign_in/auth_widget.dart
+++ lib/src/sign_in/auth_widget.dart
@@ -50,8 +50,15 @@ class AuthWidget extends StatelessWidget {
     }
 
     if (userSnapshot.hasData) {
+      // final Future<MyUser?> userFuture =
+      //     Provider.of<UserControllerImpl>(context, listen: false).getUser();
+
+      // Temporarily workaround for an authentication issue.
       final Future<MyUser?> userFuture =
-          Provider.of<UserControllerImpl>(context, listen: false).getUser();
+          Future.delayed(const Duration(milliseconds: 500), () {
+        return Provider.of<UserControllerImpl>(context, listen: false)
+            .getUser();
+      });
 
       return FutureBuilder<MyUser?>(
         future: userFuture,
