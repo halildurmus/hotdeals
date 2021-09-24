efe7468c95dd5cce1b31a61a1ed1d07020713bc3
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-09-24 13:54:33 +0300

    Use ReportUserDialog inside MessageAppBar

--- lib/src/chat/message_app_bar.dart
+++ lib/src/chat/message_app_bar.dart
@@ -6,13 +6,11 @@ import 'package:get_it/get_it.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
 
+import '../deal/report_user_dialog.dart';
 import '../models/my_user.dart';
 import '../models/user_controller_impl.dart';
-import '../models/user_report.dart';
-import '../models/user_report_reason.dart';
 import '../services/spring_service.dart';
 import '../widgets/custom_alert_dialog.dart';
-import '../widgets/loading_dialog.dart';
 
 enum _MessagePopup { blockUser, unblockUser, reportUser }
 
@@ -26,11 +24,6 @@ class MessageAppBar extends StatefulWidget {
 }
 
 class _MessageAppBarState extends State<MessageAppBar> {
-  late TextEditingController _messageController;
-  bool _harassingCheckbox = false;
-  bool _spamCheckbox = false;
-  bool _otherCheckbox = false;
-
   Future<void> _confirmBlockUser(BuildContext context) async {
     final bool _didRequestBlockUser = await CustomAlertDialog(
           title: AppLocalizations.of(context)!.blockUser,
@@ -157,156 +150,19 @@ class _MessageAppBarState extends State<MessageAppBar> {
   }
 
   @override
-  void initState() {
-    _messageController = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _messageController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _sendReport(BuildContext context, String userId) async {
-    GetIt.I.get<LoadingDialog>().showLoadingDialog(context);
-
-    final UserReport report = UserReport(
-      reportedUser: widget.user2.id!,
-      reasons: [
-        if (_harassingCheckbox) UserReportReason.harassing,
-        if (_spamCheckbox) UserReportReason.spam,
-        if (_otherCheckbox) UserReportReason.other,
-      ],
-      message:
-          _messageController.text.isNotEmpty ? _messageController.text : null,
-    );
-
-    final UserReport? sentReport =
-        await GetIt.I.get<SpringService>().sendUserReport(report: report);
-
-    // Pops the loading dialog.
-    Navigator.of(context).pop();
-    if (sentReport != null) {
-      Navigator.of(context).pop();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-            content:
-                Text(AppLocalizations.of(context)!.successfullyReportedUser)),
-      );
-    } else {
-      Navigator.of(context).pop();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(AppLocalizations.of(context)!.anErrorOccurred),
-        ),
-      );
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final double deviceWidth = MediaQuery.of(context).size.width;
     final ThemeData theme = Theme.of(context);
     final TextTheme textTheme = theme.textTheme;
     final MyUser _user = Provider.of<UserControllerImpl>(context).user!;
     final bool _isUserBlocked = _user.blockedUsers!.contains(widget.user2.uid);
 
-    Widget _buildReportDialog() {
-      return Dialog(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        ),
-        child: StatefulBuilder(
-          builder:
-              (BuildContext context, void Function(void Function()) setState) {
-            return Padding(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    AppLocalizations.of(context)!.reportUser,
-                    style: textTheme.headline6,
-                  ),
-                  const SizedBox(height: 10),
-                  CheckboxListTile(
-                    title: Text(AppLocalizations.of(context)!.harassing),
-                    value: _harassingCheckbox,
-                    onChanged: (bool? newValue) {
-                      setState(() {
-                        _harassingCheckbox = newValue!;
-                      });
-                    },
-                  ),
-                  CheckboxListTile(
-                    title: Text(AppLocalizations.of(context)!.spam),
-                    value: _spamCheckbox,
-                    onChanged: (bool? newValue) {
-                      setState(() {
-                        _spamCheckbox = newValue!;
-                      });
-                    },
-                  ),
-                  CheckboxListTile(
-                    title: Text(AppLocalizations.of(context)!.other),
-                    value: _otherCheckbox,
-                    onChanged: (bool? newValue) {
-                      setState(() {
-                        _otherCheckbox = newValue!;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 10),
-                  TextField(
-                    controller: _messageController,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      hintStyle: textTheme.bodyText2!.copyWith(
-                          color: theme.brightness == Brightness.light
-                              ? Colors.black54
-                              : Colors.grey),
-                      hintText: AppLocalizations.of(context)!
-                          .enterSomeDetailsAboutReport,
-                    ),
-                    minLines: 1,
-                    maxLines: 10,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    width: deviceWidth,
-                    child: SizedBox(
-                      height: 45,
-                      child: ElevatedButton(
-                        onPressed: _harassingCheckbox ||
-                                _spamCheckbox ||
-                                _otherCheckbox
-                            ? () => _sendReport(context, _user.id!)
-                            : null,
-                        style: ElevatedButton.styleFrom(
-                          primary: theme.colorScheme.secondary,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
-                        child: Text(AppLocalizations.of(context)!.reportUser),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          },
-        ),
-      );
-    }
-
     Future<void> _onPressedReport() async {
       showDialog<void>(
         context: context,
-        builder: (BuildContext context) {
-          return _buildReportDialog();
-        },
+        builder: (BuildContext context) => ReportUserDialog(
+          loggedInUserId: _user.id!,
+          reportedUserId: widget.user2.id!,
+        ),
       );
     }
 
