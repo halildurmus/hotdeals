c6d2dacebf966f57429bbd908e224a0d23188437
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-01 20:17:27 +0300

    Add ErrorIndicatorUtil

---
+++ lib/src/utils/error_indicator_util.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../widgets/error_indicator.dart';
+
+class ErrorIndicatorUtil {
+  static Widget buildFirstPageError(
+    BuildContext context, {
+    required VoidCallback onTryAgain,
+  }) {
+    return ErrorIndicator(
+      icon: Icons.wifi,
+      title: AppLocalizations.of(context)!.noConnection,
+      message: AppLocalizations.of(context)!.checkYourInternet,
+      onTryAgain: onTryAgain,
+    );
+  }
+
+  static Widget buildNewPageError(
+    BuildContext context, {
+    required VoidCallback onTryAgain,
+  }) {
+    return InkWell(
+      onTap: onTryAgain,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.somethingWentWrong,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 4),
+            const Icon(Icons.refresh, size: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
