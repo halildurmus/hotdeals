467aefe2cc46dd20a51a89948e7d82f4b5261507
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-06 17:21:49 +0300

    Create DateTimeUtil class

---
+++ lib/src/utils/date_time_util.dart
@@ -0,0 +1,22 @@
+import 'package:get_it/get_it.dart';
+import 'package:timeago/timeago.dart';
+
+import '../settings/settings_controller.dart';
+
+/// A static class that contains useful utility functions for formatting
+/// `DateTime` with [timeago].
+class DateTimeUtil {
+  static final String _languageCode =
+      GetIt.I.get<SettingsController>().locale.languageCode;
+
+  /// Formats the provided [dateTime] to a fuzzy time like 'a moment ago' or
+  /// much shorter 'now' depending on the [useShortMessages] parameter.
+  static String formatDateTime(
+    DateTime dateTime, {
+    bool useShortMessages = true,
+  }) =>
+      format(
+        dateTime,
+        locale: useShortMessages ? _languageCode + '_short' : _languageCode,
+      );
+}
