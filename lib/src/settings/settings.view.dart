b0792695af21512e607fb90468a3a211baca350e
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-08 14:41:51 +0300

    Refactor SettingsView

--- lib/src/settings/settings.view.dart
+++ lib/src/settings/settings.view.dart
@@ -34,17 +34,9 @@ class SettingsView extends StatelessWidget {
         false;
 
     if (_didRequestSignOut) {
-      // final MyUser user =
-      //     Provider.of<UserControllerImpl>(context, listen: false).user!;
       final String? fcmToken = await FirebaseMessaging.instance.getToken();
-      // final int index = user.fcmTokens!.indexOf(fcmToken!);
-
-      // await GetIt.I.get<SpringService>().removeFcmToken(
-      //     userId: user.id!, fcmTokenIndex: index, fcmToken: fcmToken);
       await GetIt.I.get<SpringService>().logout(fcmToken: fcmToken!);
-
       await _signOut(context);
-
       Navigator.of(context).pushReplacementNamed('/');
     }
   }
@@ -62,178 +54,168 @@ class SettingsView extends StatelessWidget {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    Widget getLanguageImage(Locale locale) {
-      late String svgLocation;
-      if (locale == kLocaleTurkish) {
-        svgLocation = kTurkishSvg;
-      } else {
-        svgLocation = kEnglishSvg;
-      }
-
-      return SvgPicture.asset(svgLocation);
+  Widget _getLanguageImage(Locale locale) {
+    late String svgLocation;
+    if (locale == kLocaleTurkish) {
+      svgLocation = kTurkishSvg;
+    } else {
+      svgLocation = kEnglishSvg;
     }
 
-    String getLanguageName(Locale locale) {
-      if (locale == kLocaleEnglish) {
-        return AppLocalizations.of(context)!.english;
-      }
+    return SvgPicture.asset(svgLocation);
+  }
 
+  String _getLanguageName(BuildContext context, Locale locale) {
+    if (locale == kLocaleTurkish) {
       return AppLocalizations.of(context)!.turkish;
     }
 
-    String getThemeName(ThemeMode themeMode) {
-      if (themeMode == ThemeMode.system) {
-        return AppLocalizations.of(context)!.system;
-      } else if (themeMode == ThemeMode.light) {
-        return AppLocalizations.of(context)!.light;
-      }
+    return AppLocalizations.of(context)!.english;
+  }
 
+  String _getThemeName(BuildContext context, ThemeMode themeMode) {
+    if (themeMode == ThemeMode.dark) {
       return AppLocalizations.of(context)!.dark;
+    } else if (themeMode == ThemeMode.light) {
+      return AppLocalizations.of(context)!.light;
     }
 
-    Future<void> changeLanguage() {
-      return showDialog<void>(
-        context: context,
-        builder: (BuildContext ctx) {
-          return StatefulBuilder(
-            builder:
-                (BuildContext context, void Function(VoidCallback) setState) {
-              return SettingsDialog(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    RadioItem<Locale>(
-                      onChanged: controller.updateLocale,
-                      onTap: () => controller.updateLocale(kLocaleEnglish),
-                      providerValue: controller.locale,
-                      radioValue: kLocaleEnglish,
-                      text: AppLocalizations.of(context)!.english,
-                      iconPath: 'assets/icons/en.svg',
-                    ),
-                    RadioItem<Locale>(
-                      onChanged: controller.updateLocale,
-                      onTap: () => controller.updateLocale(kLocaleTurkish),
-                      providerValue: controller.locale,
-                      radioValue: kLocaleTurkish,
-                      text: AppLocalizations.of(context)!.turkish,
-                      iconPath: 'assets/icons/tr.svg',
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                      height: 45,
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        onPressed: () => Navigator.of(context).pop(),
-                        child: Text(
-                          AppLocalizations.of(context)!.ok,
-                          style: textTheme.bodyText1!
-                              .copyWith(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      );
-    }
+    return AppLocalizations.of(context)!.system;
+  }
 
-    Future<void> changeAppTheme() {
-      return showDialog<void>(
-        context: context,
-        builder: (BuildContext ctx) {
-          return SettingsDialog(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                RadioItem<ThemeMode>(
-                  onChanged: controller.updateThemeMode,
-                  onTap: () {
-                    controller.updateThemeMode(ThemeMode.system);
-                  },
-                  providerValue: controller.themeMode,
-                  radioValue: ThemeMode.system,
-                  icon: Icons.brightness_auto,
-                  text: AppLocalizations.of(context)!.system,
-                ),
-                RadioItem<ThemeMode>(
-                  onChanged: controller.updateThemeMode,
-                  onTap: () {
-                    controller.updateThemeMode(ThemeMode.light);
-                  },
-                  providerValue: controller.themeMode,
-                  radioValue: ThemeMode.light,
-                  icon: Icons.light_mode,
-                  text: AppLocalizations.of(context)!.light,
-                ),
-                RadioItem<ThemeMode>(
-                  onChanged: controller.updateThemeMode,
-                  onTap: () {
-                    controller.updateThemeMode(ThemeMode.dark);
-                  },
-                  providerValue: controller.themeMode,
-                  radioValue: ThemeMode.dark,
-                  icon: Icons.dark_mode,
-                  text: AppLocalizations.of(context)!.dark,
-                ),
-                const SizedBox(height: 15),
-                SizedBox(
-                  height: 45,
-                  width: double.infinity,
-                  child: ElevatedButton(
+  Future<void> _changeLanguage(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final deviceWidth = MediaQuery.of(context).size.width;
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext ctx) {
+        return StatefulBuilder(
+          builder:
+              (BuildContext context, void Function(VoidCallback) setState) {
+            return SettingsDialog(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RadioItem<Locale>(
+                    onTap: () => controller.updateLocale(kLocaleEnglish),
+                    onChanged: controller.updateLocale,
+                    providerValue: controller.locale,
+                    radioValue: kLocaleEnglish,
+                    iconPath: 'assets/icons/en.svg',
+                    text: AppLocalizations.of(context)!.english,
+                  ),
+                  RadioItem<Locale>(
+                    onTap: () => controller.updateLocale(kLocaleTurkish),
+                    onChanged: controller.updateLocale,
+                    providerValue: controller.locale,
+                    radioValue: kLocaleTurkish,
+                    iconPath: 'assets/icons/tr.svg',
+                    text: AppLocalizations.of(context)!.turkish,
+                  ),
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () => Navigator.of(context).pop(),
                     style: ElevatedButton.styleFrom(
+                      fixedSize: Size(deviceWidth, 45),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                    onPressed: () => Navigator.of(context).pop(),
                     child: Text(
                       AppLocalizations.of(context)!.ok,
                       style: textTheme.bodyText1!.copyWith(color: Colors.white),
                     ),
                   ),
-                ),
-              ],
-            ),
-          );
-        },
-      );
-    }
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 
+  Future<void> _changeAppTheme(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final deviceWidth = MediaQuery.of(context).size.width;
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext ctx) {
+        return SettingsDialog(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              RadioItem<ThemeMode>(
+                onTap: () => controller.updateThemeMode(ThemeMode.system),
+                onChanged: controller.updateThemeMode,
+                providerValue: controller.themeMode,
+                radioValue: ThemeMode.system,
+                icon: Icons.brightness_auto,
+                text: AppLocalizations.of(context)!.system,
+              ),
+              RadioItem<ThemeMode>(
+                onTap: () => controller.updateThemeMode(ThemeMode.light),
+                onChanged: controller.updateThemeMode,
+                providerValue: controller.themeMode,
+                radioValue: ThemeMode.light,
+                icon: Icons.light_mode,
+                text: AppLocalizations.of(context)!.light,
+              ),
+              RadioItem<ThemeMode>(
+                onTap: () => controller.updateThemeMode(ThemeMode.dark),
+                onChanged: controller.updateThemeMode,
+                providerValue: controller.themeMode,
+                radioValue: ThemeMode.dark,
+                icon: Icons.dark_mode,
+                text: AppLocalizations.of(context)!.dark,
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(deviceWidth, 45),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text(
+                  AppLocalizations.of(context)!.ok,
+                  style: textTheme.bodyText1!.copyWith(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.settings)),
       body: Column(
         children: [
           SettingsListItem(
-            image: getLanguageImage(controller.locale),
+            onTap: () => _changeLanguage(context),
+            image: _getLanguageImage(controller.locale),
             title: AppLocalizations.of(context)!.language,
-            subtitle: getLanguageName(controller.locale),
-            onTap: changeLanguage,
+            subtitle: _getLanguageName(context, controller.locale),
           ),
           SettingsListItem(
+            onTap: () => _changeAppTheme(context),
             icon: Icons.settings_brightness,
             title: AppLocalizations.of(context)!.theme,
-            subtitle: getThemeName(controller.themeMode),
-            onTap: changeAppTheme,
+            subtitle: _getThemeName(context, controller.themeMode),
           ),
           if (Provider.of<UserControllerImpl>(context).user != null)
             SettingsListItem(
+              onTap: () => _confirmSignOut(context),
               hasNavigation: false,
               icon: Icons.cancel,
               title: AppLocalizations.of(context)!.logout,
-              onTap: () => _confirmSignOut(context),
             ),
         ],
       ),
