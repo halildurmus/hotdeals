f36bfc90d0470fd176f74e7f60a9f75f350cc4bc
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-10 18:57:27 +0300

    Move PagedListView inside the Notifications widget

--- lib/src/notification/notifications.dart
+++ lib/src/notification/notifications.dart
@@ -6,8 +6,9 @@ import 'package:loggy/loggy.dart' show NetworkLoggy;
 
 import '../models/push_notification.dart';
 import '../services/push_notification_service.dart';
+import '../utils/error_indicator_util.dart';
 import '../widgets/error_indicator.dart';
-import 'notification_paged_listview.dart';
+import '../widgets/notification_item.dart';
 
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
@@ -17,6 +18,7 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> with NetworkLoggy {
+  static const kPageSize = 10;
   late PagingController<int, PushNotification> _pagingController;
   late PushNotificationService _pushNotificationService;
 
@@ -24,6 +26,7 @@ class _NotificationsState extends State<Notifications> with NetworkLoggy {
   void initState() {
     _pagingController =
         PagingController<int, PushNotification>(firstPageKey: 0);
+    _pagingController.addPageRequestListener((pageKey) => _fetchPage(pageKey));
     _pushNotificationService = GetIt.I.get<PushNotificationService>();
     _pushNotificationService.addListener(() => _pagingController.refresh());
     super.initState();
@@ -35,13 +38,55 @@ class _NotificationsState extends State<Notifications> with NetworkLoggy {
     super.dispose();
   }
 
-  Future<List<PushNotification>> _notificationFuture(int offset, int limit) =>
-      _pushNotificationService.getAll(offset: offset, limit: limit);
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = await _pushNotificationService.getAll(
+          offset: pageKey, limit: kPageSize);
+      final isLastPage = newItems.length < kPageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      loggy.error(error);
+      _pagingController.error = error;
+    }
+  }
 
   Widget buildNoNotificationsFound(BuildContext context) {
     return ErrorIndicator(
       icon: Icons.notifications_none_outlined,
       title: AppLocalizations.of(context)!.noNotifications,
+    );
+  }
+
+  Widget _buildPagedListView() {
+    return PagedListView.separated(
+      pagingController: _pagingController,
+      builderDelegate: PagedChildBuilderDelegate<PushNotification>(
+        animateTransitions: true,
+        itemBuilder: (context, notification, index) =>
+            NotificationItem(notification: notification),
+        firstPageErrorIndicatorBuilder: (context) =>
+            ErrorIndicatorUtil.buildFirstPageError(
+          context,
+          onTryAgain: () => _pagingController.refresh(),
+        ),
+        newPageErrorIndicatorBuilder: (context) =>
+            ErrorIndicatorUtil.buildNewPageError(
+          context,
+          onTryAgain: () => _pagingController.refresh(),
+        ),
+        noItemsFoundIndicatorBuilder: (context) =>
+            buildNoNotificationsFound(context),
+      ),
+      separatorBuilder: (context, index) => const Divider(
+        height: 0,
+        indent: 16,
+        endIndent: 16,
+      ),
     );
   }
 
@@ -51,11 +96,9 @@ class _NotificationsState extends State<Notifications> with NetworkLoggy {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.notifications),
       ),
-      body: NotificationPagedListView(
-        notificationFuture: _notificationFuture,
-        noNotificationsFound: buildNoNotificationsFound(context),
-        pageSize: 10,
-        pagingController: _pagingController,
+      body: RefreshIndicator(
+        onRefresh: () => Future.sync(() => _pagingController.refresh()),
+        child: _buildPagedListView(),
       ),
     );
   }
