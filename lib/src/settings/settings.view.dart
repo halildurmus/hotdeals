3842d68160dd1665b3be59ff6a582460d7665653
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-17 15:31:27 +0300

    Use relative import

--- lib/src/settings/settings.view.dart
+++ lib/src/settings/settings.view.dart
@@ -2,11 +2,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:hotdeals/src/widgets/settings_section.dart';
 
 import '../constants.dart';
 import '../widgets/radio_item.dart';
 import '../widgets/settings_list_item.dart';
+import '../widgets/settings_section.dart';
 import 'settings.controller.dart';
 import 'settings_dialog.dart';
 
