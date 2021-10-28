f7688b3d15043ec407b1e270f1c96daf9a5595f1
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-10-29 00:32:30 +0300

    Create a dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,34 +1,17 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:hotdeals/src/app.dart';
-// import 'package:hotdeals/src/settings/settings_controller.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-//
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Initializes a new SharedPreferences instance.
-//     final SharedPreferences prefs = await SharedPreferences.getInstance();
-//
-//     // Sets up the SettingsController, which will glue user settings to multiple
-//     // Flutter Widgets.
-//     final SettingsController settingsController =
-//         FakeSettingsController(FakeSettingsService(prefs));
-//
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp(
-//       settingsController: settingsController,
-//     ));
-//
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-//
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-//
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hotdeals/src/app.dart';
+import 'package:hotdeals/src/settings/settings.controller.dart';
+
+import 'settings/fake_settings.service.dart';
+
+void main() {
+  testWidgets('Dummy test', (WidgetTester tester) async {
+    // Sets up the SettingsController with the FakeSettingsService.
+    // final settingsController = SettingsController(FakeSettingsService());
+
+    // Build the MyApp widget.
+    // await tester.pumpWidget(const MyApp());
+
+    expect(true, true);
+  });
+}
