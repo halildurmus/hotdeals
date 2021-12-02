6db4a9ebb4f894f30d629317b238a82cc243f280
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-02 16:57:39 +0300

    Fix extension

--- lib/src/utils/enum_util.dart
+++ lib/src/utils/enum_util.dart
@@ -1,5 +1,6 @@
+// TODO(halildurmus): Remove this once Dart 2.15 is released.
 extension EnumName on Enum {
   /// Returns the enum name.
   // ignore: recursive_getters
-  String get name => name;
+  String get name => toString().split('.').last;
 }
