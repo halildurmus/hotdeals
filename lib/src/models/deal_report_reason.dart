49fe0794e9756e2d0154cd012ab9646991d45f7b
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-02 16:58:11 +0300

    Fix String to Enum

--- lib/src/models/deal_report_reason.dart
+++ lib/src/models/deal_report_reason.dart
@@ -5,4 +5,5 @@ enum DealReportReason { expired, repost, spam, other }
 
 /// Returns the proper [DealReportReason] for the given [str].
 DealReportReason dealReportReasonFromString(String str) =>
-    DealReportReason.values.firstWhere((reason) => reason.name == str);
+    DealReportReason.values
+        .firstWhere((reason) => reason.name.toUpperCase() == str);
--- lib/src/models/user_report_reason.dart
+++ lib/src/models/user_report_reason.dart
@@ -5,4 +5,5 @@ enum UserReportReason { harassing, spam, other }
 
 /// Returns the proper [UserReportReason] for the given [str].
 UserReportReason userReportReasonFromString(String str) =>
-    UserReportReason.values.firstWhere((reason) => reason.name == str);
+    UserReportReason.values
+        .firstWhere((reason) => reason.name.toUpperCase() == str);
