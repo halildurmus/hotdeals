95333025665d7d2b06e543a729259287f5276dec
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-23 16:36:03 +0300

    Create DealUtil class

---
+++ lib/src/deal/deal_util.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_picture_uploader/firebase_picture_uploader.dart'
+    show UploadJob;
+
+class DealUtil {
+  static bool isUploadInProgress(List<UploadJob> jobs) =>
+      jobs.where((e) => e.uploadProcessing == true).isNotEmpty;
+
+  static Future<List<String>> getDownloadUrls(List<UploadJob> jobs) async =>
+      Future.wait<String>(
+          jobs.map((e) => e.storageReference!.getDownloadURL()));
+}
