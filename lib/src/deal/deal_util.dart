c05a1eedf3680903ec9dcebacf8b282c11451f1a
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2021-12-24 19:18:18 +0300

    Add loadInitialImages function

--- lib/src/deal/deal_util.dart
+++ lib/src/deal/deal_util.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_picture_uploader/firebase_picture_uploader.dart'
-    show UploadJob;
+    show UploadAction, UploadJob;
+import 'package:get_it/get_it.dart';
+
+import '../models/deal.dart';
+import '../services/firebase_storage_service.dart';
 
 class DealUtil {
   static bool isUploadInProgress(List<UploadJob> jobs) =>
@@ -9,4 +13,16 @@ class DealUtil {
       Future.wait<String>(jobs
           .where((e) => e.storageReference != null)
           .map((e) => e.storageReference!.getDownloadURL()));
+
+  static List<UploadJob> loadInitialImages({required List<String> photos}) {
+    final firebaseStorageService = GetIt.I.get<FirebaseStorageService>();
+    final uploadJobs = <UploadJob>[];
+    for (final url in photos) {
+      final uploadJob = UploadJob(action: UploadAction.actionUpload)
+        ..storageReference = firebaseStorageService.refFromURL(url: url);
+      uploadJobs.add(uploadJob);
+    }
+
+    return uploadJobs;
+  }
 }
